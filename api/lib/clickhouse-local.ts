f66e78404cf2af2c94ebd39206a3b283af800e2c
Abdou TOP <top.abdou100@outlook.fr>
2026-06-10 16:49:56 +0000

    fix: add setup task for database configuration in CI workflow

--- api/lib/clickhouse-local.ts
+++ api/lib/clickhouse-local.ts
@@ -1,7 +1,6 @@
-import { Session } from 'chdb'
-
-export function createLocalClient(path: string) {
-  const session = new Session(path)
+import defer * as chdb from 'chdb'
+export  function createLocalClient(path: string) {
+  const session = new  chdb.Session(path)
   session.query(`SET date_time_input_format = 'best_effort'`)
 
   return {
